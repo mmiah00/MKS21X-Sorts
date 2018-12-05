8f02343e92a4ee1fd3a6af418258a7d62dff3568
mmiah <mmiah00@stuy.edu>
2018-12-04 20:47:01 -0500

    wrote isSorted and selectionsort. needs to be testeD

--- Sorts.java
+++ Sorts.java
@@ -1,2 +1,25 @@
 public class Sorts {
+  public static void selectionsort (int[] ary) {
+    int smallest = ary[0];
+    int x = 0;
+    while (!isSorted (ary)) {
+       for (int i = 0; i < ary.length; i ++) {
+         if (ary[i] < smallest) {
+           smallest = ary[i];
+         }
+       }
+       ary [x] = smallest;
+       x += 1;
+    }
+  }
+
+  private static boolean isSorted (int[] ary) {
+    for (int x = 0; x + 1< ary.length; x ++) {
+      if (ary[x + 1] < ary [x] ) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
